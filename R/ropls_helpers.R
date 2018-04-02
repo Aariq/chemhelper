76f71c12dbe2d46ec214627da6ab30b93289f05f
Aariq <scottericr@gmail.com>
2018-04-02 17:47:36 -0400

    added a function to properly format CAS numbers and check their validity

--- R/ropls_helpers.R
+++ R/ropls_helpers.R
@@ -22,3 +22,30 @@ get_VIP <- function(pls){
     rownames_to_column() %>% 
     rename(Variable = "rowname", VIP = ".")
 }
+
+
+#' Format numbers correctly as CAS numbers
+#'
+#'
+#' @param x an integer or character vector
+#' @importFrom webchem is.cas
+#' @import stringr
+#' @import purrr
+#' @return 
+#' @export
+#'
+#' @examples
+#' x <- c(58082, 83341, 12345678, "hexanol")
+#' format_CAS(x)
+
+format_CAS <- function(x){
+  if(any(!str_detect(x, "^\\d+$"))){
+    warning("Some elements of x cannot be converted to CAS numbers")
+  }
+  match <- str_match(x, pattern = "([0-9]+)([0-9]{2})([0-9]{1})")
+  parsed <- paste0(match[,2], "-", match[,3], "-", match[,4])
+  pass <- map_lgl(parsed, is.cas)
+  out <- ifelse(pass, parsed, NA)
+  return(out)
+}
+
