9a64193847be8d888fd426cd153d1ec0e60c2a41
Aariq <scottericr@gmail.com>
2018-04-23 14:35:42 -0400

    added get_plotdata(), a function to get tidy data from opls objects for plotting with ggplot2

--- R/ropls_helpers.R
+++ R/ropls_helpers.R
@@ -31,7 +31,7 @@ get_VIP <- function(pls){
 #' @importFrom webchem is.cas
 #' @import stringr
 #' @import purrr
-#' @return 
+#' @return a character vector of correctly formatted CAS numbers
 #' @export
 #'
 #' @examples
@@ -49,3 +49,87 @@ format_CAS <- function(x){
   return(out)
 }
 
+#' Extract data for plotting (O)PLS(-DA) data with ggplot2
+#' 
+#' Extracts relevant data from an "opls" object for making annotated score plots with ggplot2 or other plotting packages.
+#'
+#' @param model an object created by ropls::opls()
+#' @import dplyr
+#' @import ropls
+#'
+#' @return For **PCA**, a list containing: principal component scores (`plot_data`, a data frame) and variance explained (`var_explained`), a named vector) for PC axes
+#' 
+#' For **PLS** and **PLS-DA**, a list containing: A dataframe of scores along predictive axes and y-variable values (`plot_data`), descriptive statistics for each axis (`axis_stats`), and descriptive statistics for the model including R^2, Q^2, and p-value (`model_stats`)
+#' 
+#' For **OPLS** and **OPLS-DA**, a list containing: A dataframe of scores along one predictive axis, orthogonal axes, and y-variable values (`plot_data`), descriptive statistics for each axis (`axis_stats`), and descriptive statistics for the model including R^2, Q^2, and p-value (`model_stats`)
+#' 
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' library(ropls)
+#' data(sacurine)
+#' sacurine.oplsda <- opls(sacurine$dataMatrix, sacurine$sampleMetadata[, "gender"],
+#'                         predI = 1,
+#'                         orthoI = NA)
+#' df <- get_plotdata(sacurine.oplsda)
+#' }
+
+get_plotdata <- function(model){
+  #check object type
+  if(class(model) != "opls"){
+    stop(paste("Expected a model object created by ropls::opls(), but was passed an object of class", class(model)[1]))
+  }
+  if(model@typeC == "PCA"){
+    scores <- model@scoreMN %>% as.data.frame() %>% rownames_to_column(var = "sample")
+    plot_data <- scores
+    
+    var_explained <- model@pcaVarVn
+    
+    return(list("plot_data" = plot_data,
+                "var_explained" = var_explained))
+    
+  } else if(model@typeC == "PLS-DA"){
+    y <- model@suppLs$yMCN %>% as.data.frame() %>% rownames_to_column(var = "sample")
+    scores <- model@scoreMN %>% as.data.frame() %>% rownames_to_column(var = "sample")
+    plot_data <- full_join(y, scores)
+    
+    return(list("plot_data" = plot_data,
+                "axis_stats" = model@modelDF,
+                "model_stats" = model@summaryDF)) 
+    
+  } else if(model@typeC == "OPLS-DA"){
+    #make a OPLS-DA data frame
+    pred.scores <- model@scoreMN %>% as.data.frame() %>% rownames_to_column(var = "sample")
+    ortho.scores <- model@orthoScoreMN %>% as.data.frame() %>% rownames_to_column(var = "sample")
+    scores <- full_join(pred.scores, ortho.scores)
+    y <- model@suppLs$yMCN %>% as.data.frame() %>% rownames_to_column(var = "sample")
+    plot_data <- full_join(y, scores)
+    
+    return(list("plot_data" = plot_data,
+                "axis_stats" = model@modelDF,
+                "model_stats" = model@summaryDF))
+    
+  } else if(model@typeC == "PLS"){
+    #make a PLS data frame
+    scores <- model@scoreMN %>% as.data.frame() %>% rownames_to_column(var = "sample")
+    y <- model@suppLs$yMCN %>% as.data.frame() %>% rownames_to_column(var = "sample")
+    plot_data <- full_join(y ,scores)
+    
+    return(list("plot_data" = plot_data,
+                "axis_stats" = model@modelDF,
+                "model_stats" = model@summaryDF)) 
+    
+  } else if(model@typeC == "OPLS"){
+    #make an OPLS data frame
+    pred.scores <- model@scoreMN %>% as.data.frame() %>% rownames_to_column(var = "sample")
+    ortho.scores <- model@orthoScoreMN %>% as.data.frame() %>% rownames_to_column(var = "sample")
+    scores <- full_join(pred.scores, ortho.scores)
+    y <- model@suppLs$yMCN %>% as.data.frame() %>% rownames_to_column(var = "sample")
+    plot_data <- full_join(y, scores)
+    
+    return(list("plot_data" = plot_data,
+                "axis_stats" = model@modelDF,
+                "model_stats" = model@summaryDF))
+  }
+}
