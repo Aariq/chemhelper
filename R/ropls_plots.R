ee435d16c66128e09026be8dfe196497d615de24
Aariq <scottericr@gmail.com>
2018-11-29 17:34:23 -0500

    added plot method for opls.  Seems like some things are missing documentation maybe?  Getting an error with get_VIP()

--- R/ropls_plots.R
+++ R/ropls_plots.R
@@ -14,18 +14,25 @@
 #' \dontrun{
 #' plot_pca(pca, data$treatment)
 #' }
-plot_pca <- function(ropls_pca, group_var){
+plot_pca <- function(ropls_pca, group_var, annotate = c("caption", "subtitle")){
   plotdata <- chemhelper::get_plotdata(ropls_pca)
-  ggplot(plotdata$plot_data, aes(x = p1, y = p2, color = group_var)) +
+  p <- ggplot(plotdata$plot_data, aes(x = p1, y = p2, color = group_var)) +
     geom_point() +
     stat_ellipse() +
     labs(x = paste0("PC1 (", plotdata$var_explained$R2X[1] * 100, "%)"),
          y = paste0("PC2 (", plotdata$var_explained$R2X[2] * 100, "%)")) +
     scale_colour_discrete("Group Membership") +
     theme_bw() +
-    ggtitle("PCA") +
-    labs(caption = latex2exp::TeX(
-      paste0("$R^2(cumulative) = ", max(plotdata$var_explained$`R2X(cum)`, "$"))))
+    labs(title = "PCA")
+  stats <- latex2exp::TeX(
+    paste0("$R^2(cumulative) = ", max(plotdata$var_explained$`R2X(cum)`, "$")))
+  if(annotate == "caption"){
+  p + labs(caption = stats)
+  } else if(annotate == "subtitle"){
+    p + ggtitle("PCA", subtitle = stats)
+  } else {
+    p
+  }
 }
 
 
@@ -45,20 +52,27 @@ plot_pca <- function(ropls_pca, group_var){
 #' \dontrun{
 #' plot_plsda(plsda)
 #' }
-plot_plsda <- function(ropls_plsda){
+plot_plsda <- function(ropls_plsda, annotate = c("caption", "subtitle")){
   plotdata <- chemhelper::get_plotdata(ropls_plsda)
-  ggplot(plotdata$plot_data, aes(x = p1, y = p2, color = y1)) +
+  p <- ggplot(plotdata$plot_data, aes(x = p1, y = p2, color = y1)) +
     geom_point() +
     stat_ellipse() +
     labs(x = paste0("P1 (", plotdata$axis_stats$R2X[1] * 100, "%)"),
          y = paste0("P2 (", plotdata$axis_stats$R2X[2] * 100, "%)")) +
     scale_color_discrete("Group Membership") +
     theme_bw() +
-    ggtitle("PLS-DA") +
-    labs(caption = TeX(
-      paste0("$R^{2}_{Y} = ", plotdata$model_stats$`R2Y(cum)`, "$; ",
-             "$Q^{2} = ", plotdata$model_stats$`Q2(cum)`, "$; ",
-             "$p_{Q^{2}} = ", plotdata$model_stats$pQ2, "$")))
+    labs(title = "PLS-DA")
+  stats <- latex2exp::TeX(
+    paste0("$R^{2}_{Y} = ", plotdata$model_stats$`R2Y(cum)`, "$; ",
+           "$Q^{2} = ", plotdata$model_stats$`Q2(cum)`, "$; ",
+           "$p_{Q^{2}} = ", plotdata$model_stats$pQ2, "$"))
+  if (annotate == "caption"){
+    p + labs(caption = stats)
+  } else if(annotate == "subtitle"){
+    p + labs(subtitle = stats)
+  } else{
+    p
+  }
 }
 
 #' Plot PLS regression models produced by `ropls::opls()`
@@ -76,17 +90,62 @@ plot_plsda <- function(ropls_plsda){
 #' \dontrun{
 #' plot_pls(pls)
 #' }
-plot_pls <- function(ropls_pls){
+plot_pls <- function(ropls_pls, annotate = c("caption", "subtitle")){
   plotdata <- chemhelper::get_plotdata(ropls_pls)
-  ggplot(plotdata$plot_data, aes(x = p1, y = p2, color = y1)) +
+  p <- ggplot(plotdata$plot_data, aes(x = p1, y = p2, color = y1)) +
     geom_point() +
     labs(x = paste0("P1 (", plotdata$axis_stats$R2X[1] * 100, "%)"),
          y = paste0("P2 (", plotdata$axis_stats$R2X[2] * 100, "%)")) +
     scale_color_viridis_c() +
     theme_bw() +
-    ggtitle("PLSR") +
-    labs(caption = TeX(
-      paste0("$R^{2}_{Y} = ", plotdata$model_stats$`R2Y(cum)`, "$; ",
-             "$Q^{2} = ", plotdata$model_stats$`Q2(cum)`, "$; ",
-             "$p_{Q^{2}} = ", plotdata$model_stats$pQ2, "$")))
+    labs(title = "PLSR")
+  stats <- latex2exp::TeX(
+    paste0("$R^{2}_{Y} = ", plotdata$model_stats$`R2Y(cum)`, "$; ",
+           "$Q^{2} = ", plotdata$model_stats$`Q2(cum)`, "$; ",
+           "$p_{Q^{2}} = ", plotdata$model_stats$pQ2, "$"))
+  if (annotate == "caption"){
+    p + labs(caption = stats)
+  } else if (annotate == "subtitle"){
+    p + labs(subtitle = stats)
+  } else {
+    p
+  }
+}
+
+
+
+#' Plot OPLS regression models produced by `ropls::opls()`
+#'
+#' @param ropls_pls a PLS model with a discrete Y variable produced by `ropls::opls()`
+#' @param annotate where to put the model stats
+#'
+#' @return a ggplot object
+#' @import ggplot2
+#' @import latex2exp
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' plot_opls(opls)
+#' }
+plot_opls <- function(ropls_pls, annotate = c("caption", "subtitle")){
+  plotdata <- chemhelper::get_plotdata(ropls_pls)
+  p <- ggplot(plotdata$plot_data, aes(x = p1, y = o1, color = y1)) +
+    geom_point() +
+    labs(x = paste0("P1 (", plotdata$axis_stats$R2X[1] * 100, "%)"),
+         y = paste0("P2 (", plotdata$axis_stats$R2X[2] * 100, "%)")) +
+    scale_color_viridis_c() +
+    theme_bw() +
+    labs(title = "OPLSR")
+  stats <- latex2exp::TeX(
+    paste0("$R^{2}_{Y} = ", plotdata$model_stats$`R2Y(cum)`, "$; ",
+           "$Q^{2} = ", plotdata$model_stats$`Q2(cum)`, "$; ",
+           "$p_{Q^{2}} = ", plotdata$model_stats$pQ2, "$"))
+  if (annotate == "caption"){
+    p + labs(caption = stats)
+  } else if (annotate == "subtitle"){
+    p + labs(subtitle = stats)
+  } else {
+    p
+  }
 }
